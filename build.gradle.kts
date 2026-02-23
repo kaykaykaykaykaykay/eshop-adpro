23aa44986bc9e546269663e8ea5c45141074f5c3
kaykaykaykaykaykay <khayrukartajaya360@gmail.com>
2026-02-23 20:57:52 +0700

    added sonarcloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,13 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.5.10"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
 description = "eshop"
+
 val seleniumJavaVersion = "4.14.1"
 val seleniumJupiterVersion = "5.0.1"
 val webdrivermanagerVersion = "5.6.3"
@@ -19,29 +21,44 @@ java {
 	}
 }
 
+repositories {
+	mavenCentral()
+}
+
 configurations {
 	compileOnly {
 		extendsFrom(configurations.annotationProcessor.get())
 	}
 }
 
-repositories {
-	mavenCentral()
-}
-
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 	testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
 	testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
 	testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
-	testImplementation("org.junit.jupiter:junit-jupiter:${junitJupiterVersion}")
+	testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+}
+
+tasks.withType<Test>().configureEach {
+	useJUnitPlatform()
+}
+
+tasks.test {
+	filter {
+		excludeTestsMatching("*FunctionalTest")
+	}
+	finalizedBy(tasks.jacocoTestReport)
 }
 
 tasks.register<Test>("unitTest") {
@@ -49,8 +66,10 @@ tasks.register<Test>("unitTest") {
 	group = "verification"
 
 	filter {
-		excludeTestsMatching("*FuncionalTest")
+		excludeTestsMatching("*FunctionalTest")
 	}
+
+	useJUnitPlatform()
 }
 
 tasks.register<Test>("functionalTest") {
@@ -60,19 +79,29 @@ tasks.register<Test>("functionalTest") {
 	filter {
 		includeTestsMatching("*FunctionalTest")
 	}
-}
 
-tasks.withType<Test>().configureEach {
 	useJUnitPlatform()
 }
 
-tasks.test{
-	filter {
-		excludeTestsMatching("*FunctionalTest")
-	}
-	finalizedBy(tasks.jacocoTestReport)
+jacoco {
+	toolVersion = "0.8.11"
 }
 
 tasks.jacocoTestReport {
 	dependsOn(tasks.test)
+
+	reports {
+		xml.required.set(true)
+		html.required.set(true)
+		csv.required.set(false)
+	}
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "kaykaykaykaykaykay_eshop-adpro")
+		property("sonar.organization", "kaykaykaykaykaykay")
+		property("sonar.host.url", "https://sonarcloud.io")
+		property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+	}
 }
