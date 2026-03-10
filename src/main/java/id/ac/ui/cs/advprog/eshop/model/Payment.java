e9c2bcd954bb08e2238cbb4682fa11492a1b6e7d
kaykaykaykaykaykay <khayrukartajaya360@gmail.com>
2026-03-10 13:12:29 +0700

    [GREEN] Implement payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import java.util.Map;
+import java.util.List;
 @Getter
 public class Payment {
     private String id;
@@ -10,8 +11,16 @@ public class Payment {
     private Map<String, String> paymentData;
 
     public Payment(String id, Order order, String method, Map<String, String> paymentData) {
+        this.id = id;
+        this.order = order;
+        this.method = method;
+        this.paymentData = paymentData;
+        this.status = "WAITING_PAYMENT";
     }
 
     public void setStatus(String status) {
+        if (List.of("WAITING_PAYMENT", "SUCCESS", "REJECTED").contains(status)) {
+            this.status = status;
+        }
     }
 }
