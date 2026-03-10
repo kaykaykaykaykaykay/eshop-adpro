8d1fd3568bfbb6772b299aee219e5923df35c5d9
kaykaykaykaykaykay <khayrukartajaya360@gmail.com>
2026-03-10 10:50:21 +0700

    [GREEN] Implement Order model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,12 +1,9 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Getter;
-import lombok.Setter;
-
 import java.util.List;
 
 @Getter
-@Setter
 public class Order {
     private String id;
     private List<Product> products;
@@ -15,8 +12,28 @@ public class Order {
     private String status;
 
     public Order(String id, List<Product> products, Long orderTime, String author) {
+        this(id, products, orderTime, author, "WAITING_PAYMENT");
     }
 
     public Order(String id, List<Product> products, Long orderTime, String author, String status) {
+        if (products == null || products.isEmpty()) {
+            throw new IllegalArgumentException("Order must have at least one product");
+        }
+        this.id = id;
+        this.products = products;
+        this.orderTime = orderTime;
+        this.author = author;
+
+        if (List.of("WAITING_PAYMENT", "FAILED", "CANCELLED", "SUCCESS").contains(status)) {
+            this.status = status;
+        } else {
+            this.status = "WAITING_PAYMENT";
+        }
+    }
+
+    public void setStatus(String status) {
+        if (List.of("WAITING_PAYMENT", "FAILED", "CANCELLED", "SUCCESS").contains(status)) {
+            this.status = status;
+        }
     }
 }
